28873f6e66d4099f4b446616ec9cc0483c894685
AJ Alt <aj@alt>
2013-03-14 17:00:09 -0400

    Added documentation and fixed a few bugs.

--- src/optimizer.py
+++ src/optimizer.py
@@ -1,6 +1,14 @@
 '''Perform optimizations on an AST.
 
-The optimizer assumes the AST is both syntactically and semanctically valid.'''
+The optimize_tree function is the main interface to the optimizer, which will
+optimize an AST in place. The function can optimize at two different levels.
+
+Level 1: Minimal optimization is performed. The resulting tree is guaranteed to
+be quivalent to the original.
+
+Level 2: More extensive optimization is performed that may produce incorrect code.
+
+The optimizer assumes the AST is both syntactically and semantically valid.'''
 
 import itertools
 
@@ -18,6 +26,7 @@ class ConstantFolder(syntaxtree.TreeMutator):
         }
         
     def get_const(self, node):
+        '''Return the value of a constant AST node, or None if the node is not a number.'''
         if isinstance(node, syntaxtree.Num):
             return node.n
         return None
@@ -26,18 +35,20 @@ class ConstantFolder(syntaxtree.TreeMutator):
         return isinstance(node, (syntaxtree.Num, syntaxtree.Str))
     
     def visit_binary_op(self, node):
-        # Fold children first to allow for partial folding of an expression.
+        # Fold children first so that we can fold parts of an expression even if
+        # the entire expression is not constant.
         self.visit_children(node)
         
         left = self.get_const(node.left)
-        right = self.get_const(node.right)
-        if left is not None and right is not None:
-            result = eval('%s %s %s' % (left, node.op, right))
-            if result is True:
-                return syntaxtree.Num('1')
-            if result is False:
-                return syntaxtree.Num('0')
-            return syntaxtree.Num(str(result))
+        if left is not None:
+            right = self.get_const(node.right)
+            if right is not None:
+                result = eval('%s %s %s' % (left, node.op, right))
+                if result is True:
+                    return syntaxtree.Num('1')
+                if result is False:
+                    return syntaxtree.Num('0')
+                return syntaxtree.Num(str(result))
         return node
     
     def visit_unary_op(self, node):
@@ -49,14 +60,28 @@ class ConstantFolder(syntaxtree.TreeMutator):
         return node
 
 class ConstantPropagator(ConstantFolder):
-    '''Optimize that performs both constant folding and propagation.'''
+    '''Optimize that performs both constant folding and propagation.
+    
+    This optimizer can detect some uses of uninitialized variables and will
+    optionally report them as errors.
+    
+    This optimizer combines constant folding and propagation into a single pass
+    over the tree. By combining the two operations, we can save several walks
+    over the tree that would be necessary if the two were interleaved.
+    
+    Instead of taking the time to construct explicit U-D chains for the
+    propagation, we can simply record the constant value of known variables in a
+    symbol table and invalidate that value if we reach a non-constant
+    assignment. Since we do the propagation inline with folding, and the walk is
+    in program order, this will produce correct code as long as we don't miss
+    any invalidations.'''
     def __init__(self, print_errors=False):
         super(ConstantFolder, self).__init__()
         
         self.print_errors = print_errors
         
         # Scopes store the value of variables known to be constant, and the
-        # declarations of procedures, so that we can tell which paratmters are in/out
+        # declarations of procedures, so that we can tell which parameters are in/out
         self.global_scope = {}
         self.scopes = [{}]
         
@@ -72,7 +97,10 @@ class ConstantPropagator(ConstantFolder):
         }
         
         # Use a stack to keep track of whether we're in a loop or not since we
-        # can have nested loops.
+        # can have nested blocks. If there is a value on this stack, all
+        # assignments will result in an invalidation. This is more conservative
+        # than necessary, but without more reaching definition analysis, we
+        # can't tell if an assignment is loop-invariant.
         self.stop_propagation = []
         
     def enter_scope(self):
@@ -99,9 +127,8 @@ class ConstantPropagator(ConstantFolder):
             try:
                 return self.global_scope[key]
             except KeyError:
-                # if we get here, it means an uninitialized variable was used.
                 if self.print_errors:
-                    msg = 'Uninitialzed variable referenced'
+                    msg = 'Uninitialized variable referenced'
                     if name.token:
                         underline = '^' if name.token.start == name.token.end else '~'
                         line = name.token.line.rstrip()
@@ -141,7 +168,7 @@ class ConstantPropagator(ConstantFolder):
         # Add this declaration to its own scope to allow recursion.
         self.define_variable(node.name, node)
         
-        # Add parameters to so they don't get flaged as unitialized reference errors.
+        # Add parameters to so they don't get flagged as uninitialized reference errors.
         for param in node.params:
             self.define_variable(param.var_decl.name, None)
             
@@ -183,7 +210,15 @@ class ConstantPropagator(ConstantFolder):
         return node
     
 class DeadCodeEliminator(syntaxtree.TreeMutator):
-    UNKNOWN = 0
+    '''Optimizer that elimiates dead branches, loops, assignemnts, and declarations.
+    
+    This optimizer does not construct explicit D-U chains or SSA structures.
+    Instead, it works by walking the AST in reverse program order, which is a
+    top-down, right-to-left walk of the AST, marking variables as used or
+    assigned as it encounters them. When it reaches an assignment or
+    declaration, it will have encountered all references to that identifier
+    already.
+    '''
     ASSIGNED = 1
     REFERENCED = 2
     
@@ -203,6 +238,7 @@ class DeadCodeEliminator(syntaxtree.TreeMutator):
             syntaxtree.Call: self.visit_call,
             syntaxtree.If: self.visit_if,
             syntaxtree.For: self.visit_for,
+            syntaxtree.VarDecl: self.visit_vardecl,
         }
         
         
@@ -212,7 +248,7 @@ class DeadCodeEliminator(syntaxtree.TreeMutator):
     def leave_scope(self):
         self.scopes.pop()
 
-    def define_variable(self, name, value, is_global=False):
+    def define_var(self, name, value, is_global=False):
         if is_global:
             self.global_scope[name] = value
         else:
@@ -224,7 +260,7 @@ class DeadCodeEliminator(syntaxtree.TreeMutator):
         elif name in self.global_scope:
             self.global_scope[name] = value
         # It's ok if we try to set undefined variables: we never define
-        # procedure parameters since they can't be removed.
+        # procedure parameters since they can't be eliminated.
             
     def get_var(self, name):
         if isinstance(name, syntaxtree.Name):
@@ -251,46 +287,61 @@ class DeadCodeEliminator(syntaxtree.TreeMutator):
         
             
     def visit_block(self, node):
+        # This function is used for both Program and ProcDecl nodes
+        
+        if isinstance(node, syntaxtree.ProcDecl):
+            if self.get_var(node.name)[1]  is None:
+                return None
+        
         self.enter_scope()
         
         if isinstance(node, syntaxtree.ProcDecl):
-            self.define_variable(node.name, (node, None))
+            self.define_var(node.name, (node, None))
             
         for decl in node.decls:
             if isinstance(decl, syntaxtree.ProcDecl):
-                self.define_variable(decl.name, (decl, None), decl.is_global)
+                self.define_var(decl.name, (decl, None), decl.is_global)
             else:
-                self.define_variable(decl.name, None)
+                self.define_var(decl.name, None)
         
         self.walk_body(node)
+        self.walk_body(node, 'decls')
         
-        def referenced(decl):
-            value = self.get_var(decl.name)
-            assigned = (value[1] if isinstance(value, tuple) else value) == self.ASSIGNED
-            return assigned
-        node.decls = [decl for decl in node.decls if referenced(decl)]
-        
+        # If there's a return in the body, it's isn't in a branch, so it always
+        # terminates the procedure.
+        try:
+            del node.body[node.body.index(tokens.RETURN):]
+        except ValueError:
+            pass
+
         self.leave_scope()
     
         return node
     
+    def visit_vardecl(self, node):
+        if self.get_var(node.name) is None:
+            return None
+        return node
+    
     def visit_assign(self, node):
         if self.get_var(node.target) == self.REFERENCED:
-            self.define_variable(node.target, self.ASSIGNED)
+            self.define_var(node.target, self.ASSIGNED)
             return node
         return None
 
     def visit_name(self, node):
-        self.define_variable(node, self.REFERENCED)
+        self.define_var(node, self.REFERENCED)
+        return node
         
     def visit_call(self, node):
         decl = self.get_var(node.func)[0]
-        self.define_variable(node.func, (decl, self.REFERENCED))
-        
+        self.define_var(node.func, (decl, self.REFERENCED))
         for arg, param in itertools.izip(node.args, decl.params):
             if isinstance(arg, syntaxtree.Name):
-                self.define_variable(arg, self.REFERENCED if param.direction ==
+                self.define_var(arg, self.REFERENCED if param.direction ==
                                             tokens.IN else self.ASSIGNED)
+                
+        return node
         
     def visit_if(self, node):
         if node.test == syntaxtree.Num('1'):
@@ -300,13 +351,21 @@ class DeadCodeEliminator(syntaxtree.TreeMutator):
             self.walk_body(node, 'orelse')
             # The orelse can be empty, which removes this node.
             return node.orelse
-        self.walk_body(node)
+        if not node.body and not node.orelse:
+            # In the only recorded case of the lack of functions being helpful,
+            # expressions can't have side effects, which means we can drop the
+            # test without worrying about what it's doing.
+            return None
         self.walk_body(node, 'orelse')
+        self.walk_body(node)
+        self._visit(node.test)
+        
         return node
     
     def visit_for(self, node):
         if node.test == syntaxtree.Num('0'):
             return None
+        self._visit(node.test)
         self.walk_body(node)
         return node
     
@@ -327,49 +386,20 @@ def optimize_tree(ast, level=1):
                 return
     
 if __name__ == '__main__':
+    import argparse
     import scanner
     import parser
     import typechecker
+
+    argparser = argparse.ArgumentParser(description='Test the type optimization functionality.')
     
-    src = '''
-    program test_program is
-    int a;
-    int b;
-    int c;
-    int r;
-    begin
-        a := 15 + 15;
-        b := 9 - a / (2 + 3);
-        c :=  b * 4;
-        if (c > 10) then
-            c := c - 10;
-        end if;
-        r := c * (60 / a);
-    end program
-    '''
-    
-    src = '''
-    program test_program is
-    int a;
-    int b;
-    procedure f(int x out, int y in)
-    begin
-        x := y+1;
-        f(y,y);
-    end procedure;
-    begin
-        a := 1;
-        b := 2;
-        if (a == 0) then
-            a := b;
-        else
-            b := a;
-        end if;
-    end program
-    '''
-    
-    ast = parser.parse_tokens(scanner.tokenize_string(src))
+    argparser.add_argument('filename', help='the file to parse')
+    argparser.add_argument('-O', type=int, choices=[0, 1, 2], default=2,
+                           help='the level of optimizaion to apply to the program (default 2)')
+    args = argparser.parse_args()
+    ast = parser.parse_tokens(scanner.tokenize_file(args.filename))
     if typechecker.tree_is_valid(ast):
-        optimize_tree(ast, 2)
+        optimize_tree(ast, args.O)
         syntaxtree.dump_tree(ast)
+
     
